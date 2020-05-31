334b21a25d1acdd1a37f6bfb2588ba68df930f4e
Kolch <alex-k99@mail.ru>
2020-05-31 20:02:06 +0300

    Initial Commit

---
+++ Traveling bucket list/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Traveling bucket list
+//
+//  Created by Kolchedantsev Alexey on 31.05.2020.
+//  Copyright © 2020 alex kolhc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
